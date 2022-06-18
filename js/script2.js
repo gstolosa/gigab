7398866af1d7c8b7a52538ed78e022a620cf787c
gabrielgiusti73 <gabrielgiusti@yahoo.com.ar>
2022-06-18 08:13:54 -0300

    agregando tuti

---
+++ js/script2.js
@@ -0,0 +1,92 @@
+const contenedor = document.getElementById("test");
+const botonRes = document.getElementById("boton");
+const resultadoTest = document.getElementById("resultado");
+
+const preguntas = [
+  {
+    pregunta: "1. Que juego deportivo estamos presentando?",
+    respuestas: {
+      a: "Grid Legends",
+      b: "Rocket League",
+      c: "F1 2022",
+    },
+    respuestaCorrecta: "b",
+  },
+  {
+    pregunta: "2. Que juego multiplayer shother estamos presentando?",
+    respuestas: {
+      
+      a: "Valorant",
+      b: "Modern Warfare",
+      c: "Battlefield V",
+    },
+    respuestaCorrecta: "a",
+  },
+  {
+    pregunta: "3. Cuál es el Nuevo parche lanzado?",
+    respuestas: {
+      a: "Among US",
+      b: "Fifa 21",
+      c: "MotoGP 2021",
+      d: "Sensible Soccer",
+    },
+    respuestaCorrecta: "a",
+  },
+  
+];
+
+function mostrarTest() {
+  const preguntasYrespuestas = [];
+
+  preguntas.forEach((preguntaActual, numeroDePregunta) => {
+    const respuestas = [];
+
+    for (letraRespuesta in preguntaActual.respuestas) {
+      respuestas.push(
+        `<label>
+                  <input type="radio" name="${numeroDePregunta}" value="${letraRespuesta}" />
+                  ${letraRespuesta} : ${preguntaActual.respuestas[letraRespuesta]}
+              </label>`
+      );
+    }
+
+    preguntasYrespuestas.push(
+      `<div class="cuestion">${preguntaActual.pregunta}</div>
+          <div class="respuestas"> ${respuestas.join("")} </div>
+          `
+    );
+  });
+
+  contenedor.innerHTML = preguntasYrespuestas.join("");
+}
+
+mostrarTest();
+
+function mostrarResultado() {
+  const respuestas = contenedor.querySelectorAll(".respuestas");
+  let respuestasCorrectas = 0;
+
+  preguntas.forEach((preguntaActual, numeroDePregunta) => {
+    const todasLasRespuestas = respuestas[numeroDePregunta];
+    const checkboxRespuestas = `input[name='${numeroDePregunta}']:checked`;
+    const respuestaElegida = (
+      todasLasRespuestas.querySelector(checkboxRespuestas) || {}
+    ).value;
+
+    if (respuestaElegida === preguntaActual.respuestaCorrecta) {
+      respuestasCorrectas++;
+
+      respuestas[numeroDePregunta].style.color = "blue";
+    } else {
+      respuestas[numeroDePregunta].style.color = "red";
+    }
+  });
+
+  resultadoTest.innerHTML =
+    "Usted ha acertado " +
+    respuestasCorrectas +
+    " preguntas de un total de " +
+    preguntas.length;
+}
+
+botonRes.addEventListener("click", mostrarResultado);
--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
-
-ScrollReveal().reveal('.welcome');
-ScrollReveal().reveal('.news-cards', {delay: 500});
-ScrollReveal().reveal('.social', {delay: 500});
-ScrollReveal().reveal('.footer', {delay: 500});
